4864f7a886b6abf4dd2edfc352d9b26201611969
Matheus <matheuslopesfrazao@outlook.com>
2019-08-25 11:49:07 -0300

    Criando primeiro componente

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components';
+
+
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule /* Todos os módulos que são comuns, ou seja, que não é o principal, devem ter essa declaração de "CommonModule" */
-  ]
+  declarations: [CalculadoraComponent], /* Aqui ficam os componentes que fazem parte do módulo*/
+  imports: [CommonModule /* Todos os módulos que são comuns, ou seja, que não é o principal, devem ter essa declaração de "CommonModule" */],
+  exports: [CalculadoraComponent]
 })
 export class CalculadoraModule { }
