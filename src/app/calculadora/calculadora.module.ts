f59dfd2eae18d860bb14030181224adfed15aec2
Matheus <matheuslopesfrazao@outlook.com>
2019-08-24 23:11:25 -0300

    Criando o módulo Calculadora

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule /* Todos os módulos que são comuns, ou seja, que não é o principal, devem ter essa declaração de "CommonModule" */
+  ]
+})
+export class CalculadoraModule { }
