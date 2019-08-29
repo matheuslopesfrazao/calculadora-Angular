4823f965ad0d9e897b1253ba67406f8b768dbd16
Matheus <matheuslopesfrazao@outlook.com>
2019-08-29 20:38:42 -0300

    Criando o Serviço

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './components';
-
+import { CalculadoraService } from './services/calculadora.service';
 
 
 
@@ -9,6 +9,7 @@ import { CalculadoraComponent } from './components';
 @NgModule({
   declarations: [CalculadoraComponent], /* Aqui ficam os componentes que fazem parte do módulo*/
   imports: [CommonModule /* Todos os módulos que são comuns, ou seja, que não é o principal, devem ter essa declaração de "CommonModule" */],
-  exports: [CalculadoraComponent]
+  exports: [CalculadoraComponent],
+  providers: [CalculadoraService]
 })
 export class CalculadoraModule { }
--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CalculadoraService } from '../services/calculadora.service';
 
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
 })
+
+/* A injeção de dependências é feita dentro do construtor */
 export class CalculadoraComponent implements OnInit {
 
-  constructor() { }
+  constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit() {
   }
